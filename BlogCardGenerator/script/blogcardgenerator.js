d7c027eaafb6e047e619d50246b00bf1164fa239
pq <pq@pq-VirtualBox>
2019-05-26 01:10:36 +0900

    HM

--- BlogCardGenerator/script/blogcardgenerator.js
+++ BlogCardGenerator/script/blogcardgenerator.js
@@ -1,4 +1,5 @@
-//<script>
+import onCopy from "./oncopy.js";
+
 const bookmarkletfunc = () => {
 	const mydomain = "p--q.blogspot.com";  // 引用符をつけないドメイン。
 	let elem; // 要素を入れる変数。
@@ -14,23 +15,23 @@ const bookmarkletfunc = () => {
 		ogp.img = location.protocol + "//" + ogp.domain + ogp.img;
 	}
 	let root = createTree([
-					[createElem("div", {"class": "blogcard"}, ""),
-						[createElem("div", {"class": "blogcard-content"}, ""),
-							[createElem("div", {"class": "blogcard-text"}, ""),
-								createElem("div", {"class": "blogcard-title"}, ""),
+					[createElem("div", {"class": "blogcard"}),
+						[createElem("div", {"class": "blogcard-content"}),
+							[createElem("div", {"class": "blogcard-text"}),
+								createElem("div", {"class": "blogcard-title"}),
 									createElem("a", {"href": ogp.url, "target": "_blank"}, ogp.title)
 							],
-								createElem("blockquote", {"cite": ogp.url}, ""),
+								createElem("blockquote", {"cite": ogp.url}),
 									createElem("div", {"class": "blogcard-description"}, ogp.desp)
 						],
-							createElem("div", {"class": "blogcard-image"}, ""),
-								createElem("div", {"class": "blogcard-image-wrapper"}, ""),
-									createElem("a", {"href": ogp.url, "target": "_blank"}, ""),
-										createElem("img", {"alt": "", "height": "132", "width": "200", "src": ogp.img}, "")
+							createElem("div", {"class": "blogcard-image"}),
+								createElem("div", {"class": "blogcard-image-wrapper"}),
+									createElem("a", {"href": ogp.url, "target": "_blank"}),
+										createElem("img", {"alt": "", "height": "132", "width": "200", "src": ogp.img})
 					],
-						createElem("div", {"class": "blogcard-footer"}, ""),
-							[createElem("a", {"href": ogp.domain, "target": "_blank"}, ""),
-								createElem("img", {"alt": "", "src": "https://www.google.com/s2/favicons?domain={}".replace("{}", ogp.domain)}, "")
+						createElem("div", {"class": "blogcard-footer"}),
+							[createElem("a", {"href": ogp.domain, "target": "_blank"}),
+								createElem("img", {"alt": "", "src": "https://www.google.com/s2/favicons?domain={}".replace("{}", ogp.domain)})
 							],
 								createTxtNode(ogp.domain)
 				]) // ルートノードから始まる、ツリーにするノードの配列。サムネイル画像サイズは決め打ちしている。
@@ -47,11 +48,17 @@ const bookmarkletfunc = () => {
 		elem.parentNode.insertBefore(elem.firstElementChild, elem);  // <blockquote>の子要素を親要素に付け替える。
 		elem.parentNode.removeChild(elem);  // <blockquote></blockquote>を削除。
 	}
-	document.addEventListener("copy", onCopy, true);  // targetは何でもよいので、capture phase、つまりドキュメントから行きの伝播を捉える。
-	document.execCommand("copy");   // コピーイベントを発生させる。
-	document.removeEventListener("copy", onCopy, true);  // リスナーの除去。
+	
+	
+
+	
+	onCopy(root.outerHTML);  // ツリーをHTML文字列に変換する。
+	
+//	document.addEventListener("copy", onCopy, true);  // targetは何でもよいので、capture phase、つまりドキュメントから行きの伝播を捉える。
+//	document.execCommand("copy");   // コピーイベントを発生させる。
+//	document.removeEventListener("copy", onCopy, true);  // リスナーの除去。
 	function getOGP(txt){return document.evaluate("//meta[@property='og:{}']/@content".replace("{}", txt), document, null, XPathResult.STRING_TYPE, null).stringValue;}
-	function createElem(tag, props, txt){  // タグ名、プロパティの辞書、テキストノードにするテキスト。
+	function createElem(tag, props, txt=""){  // タグ名、プロパティの辞書、テキストノードにするテキスト。
 		let elem = document.createElement(tag);  // 要素を作成。
 		Object.keys(props).forEach(k => elem.setAttribute(k, props[k]));  // 要素のプロパティを設定。				
 		txt && elem.appendChild(createTxtNode(txt));  // テキストノードがあれば挿入。		
@@ -74,13 +81,5 @@ const bookmarkletfunc = () => {
 					 }); 			
 		return root  // 木のルートを返す。
 	}
-	function onCopy(ev) {  // イベントリスナー。	
-		let html = root.outerHTML;  // ツリーをHTML文字列に変換する。
-		ev.clipboardData.setData("text/plain", html);  // textとしてペーストするとき。必須。これがないとクリップボードに何も渡されない。
-		ev.clipboardData.setData("text/html", html);  // htmlとしてペーストするとき。
-		ev.preventDefault();  // デフォルトの動作を止める。そうしないとクリップボードに元の値が入ってしまう。
-		ev.stopPropagation();  // これよりイベントの伝播を止める。
-	}
+
 };
-//</script>
-//<a href="javascript:bookmarkletfunc();">ブックマークレットを実行</a>
